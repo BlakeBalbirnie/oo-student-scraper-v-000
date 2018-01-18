93834061251f10c013399ef9cafd95d90d558dc1
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-18 15:19:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,22 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student = self
+    student_hash.each {|k,v| student.send("#{k}=", v)}
+    self.class.all << student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| self.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|k,v|self.send("#{k}=",v)}
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
