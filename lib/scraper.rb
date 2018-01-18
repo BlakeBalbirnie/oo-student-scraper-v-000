718821f1035e62e850549489ded8ac7c10ae7353
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-18 15:15:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,46 +1,46 @@
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
-  attr_accessor :index_url
-
-  @index_url = Nokogiri::HTML(File.read('fixtures/student-site/index.html'))
 
   def self.scrape_index_page(index_url)
-    student_index_array = []
-    @index_url.css(".student-card").each do |student|
-      # binding.pry
-      student_index_array << student_hash = {
-        name: student.css(".card-text-container h4.student-name").text,
-        location: student.css(".card-text-container p.student-location").text,
-        profile_url: student.css("a").attribute("href").value
-      }
-    end
-    student_index_array
-  end
-
-  def self.scrape_profile_page(profile_url)
-    profile = Nokogiri::HTML(File.read(profile_url))
-    student_profile_hash = {}
-    profile.css(".main-wrapper .social-icon-container a").each do |link|
-      url_string = link.attribute("href").value
-      if url_string.include?("twitter.com")
-        student_profile_hash[:twitter] = url_string
-      elsif url_string.include?("linkedin.com")
-        student_profile_hash[:linkedin] = url_string
-      elsif url_string.include?("github.com")
-        student_profile_hash[:github] = url_string
-      elsif url_string.include?("youtube.com")
-        student_profile_hash[:youtube] = url_string
-      elsif !(url_string.nil?)
-        student_profile_hash[:blog] = url_string
+    # this will open up the webpage to text, and pass the url into argument
+    index_page = Nokogiri::HTML(open(index_url))
+    # always set the subject to an empty array
+    students = []
+    # select the highest css class for the information you want,
+    # in this case it is the profile card that holds all the student info
+    # iterate over the profile cards and name each object 'card'
+    index_page.css(".roster-cards-container").each do |card|
+    # now inside the object 'card' iterate over each student
+      # now in 'student', pull the 4 details you need
+      card.css(".student-card a").each do |student|
+        #  set a name for each info you need
+        # .attr is a scraping tool
+        # The function returns the value of a specified element on a selected element. As with all other scraping, you will be scraping the data to a list or variable.
+        # make sure to pass it with an argument that is specific to the info you want
+        student_profile_link = "#{student.attr('href')}"
+        student_name = student.css('.student-name').text
+        student_location = student.css('.student-location').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
     end
-    student_profile_hash[:profile_quote] = profile.css(".vitals-text-container .profile-quote").text
-    student_profile_hash[:bio] = profile.css(".details-container .bio-block .bio-content .description-holder p").text
-    # binding.pry
-    student_profile_hash
+    students
   end
+end
+
+def self.scrape_profile_page(profile_slug)
+  student = {}
+  profile_page = Nokogiri::HTML(profile)slug))
+  links = profile_page.css("social-icon-container").children.css("a").map do |url|
+    url.each do |links|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("github")
+        student[:github] = link
+      end
 
 end
